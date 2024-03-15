dba3682712280704dbf1e9b3d68e7fbb5aae95a4
Ratandeep11 <ratandeep1151.be21@chitkarauniversity.edu.in>
2024-03-15 23:11:09 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database'
+
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCwMBwa38SUrXEvJfb5KRKf90YyRoyt15c",
+  authDomain: "projectnametoshfirebase.firebaseapp.com",
+  databaseURL: "https://projectnametoshfirebase-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "projectnametoshfirebase",
+  storageBucket: "projectnametoshfirebase.appspot.com",
+  messagingSenderId: "539281283499",
+  appId: "1:539281283499:web:7034e336b73cf371285aee",
+  measurementId: "G-8CR6KGN42T"
+};
+
+const fireDb = firebase.initializeApp(firebaseConfig);
+export default fireDb.database().ref();
+
+
+
+
+
+
